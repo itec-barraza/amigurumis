7934870f162405b9c42636d81dec7fe218645281
itec-barraza <63619807+itec-barraza@users.noreply.github.com>
2020-08-07 00:47:47 -0300

    Fix SCSS

--- slider.js
+++ slider.js
@@ -1,6 +1,17 @@
-$('.slider').glide({
-    autoplay: false,
-    arrowsWrapperClass: 'slider-arrows',
-    arrowRightText: '',
-    arrowLeftText: ''
+$(".right").on("click", function () {
+    console.log("right");
+    $(".slide")
+      .siblings(".active:not(:last-of-type)")
+      .removeClass("active")
+      .next()
+      .addClass("active");
   });
+  
+  $(".left").on("click", function () {
+    $(".slide")
+      .siblings(".active:not(:first-of-type)")
+      .removeClass("active")
+      .prev()
+      .addClass("active");
+  });
+  
